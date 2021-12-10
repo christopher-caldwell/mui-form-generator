52b767042ea742edc99887971c64794bf6a57d59
Christopher Caldwell <62559469+christopher-caldwell@users.noreply.github.com>
2021-12-10 09:45:39 -0700

    fix: Cleaning up titles

--- examples/next/pages/_document.tsx
+++ examples/next/pages/_document.tsx
@@ -9,6 +9,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
+          <title>MUI Form Generator | Next.JS</title>
           <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap' />
         </Head>
         <body>
